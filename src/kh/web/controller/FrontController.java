fb38c1b09d215baeb0cf991f5010d2d8256f3e53
DongWoo Lee <ehdrod12@gmail.com>
2018-06-12 12:11:09 +0900

    index.jsp 수정함
    내용 참조

--- src/kh/web/controller/FrontController.java
+++ src/kh/web/controller/FrontController.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
